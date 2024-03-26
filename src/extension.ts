5c81c748c18401c0ae6aedacdf322aa292d9d3f4
Brian Ingles <brianingles@deephaven.io>
2024-03-26 07:49:43 -0500

    Send command to session

--- src/extension.ts
+++ src/extension.ts
@@ -27,7 +27,7 @@ export function activate(context: vscode.ExtensionContext) {
       const cn = await client.getAsIdeConnection();
       const ide = await cn.startSession(type);
 
-      console.log("cn:", ide.runCode("print('Hello, world!')"));
+      ide.runCode("print('Hello, world!')");
 
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
