8f90d1bd25607c8b06f507530bfaae5b379702e2
Brian Ingles <brianingles@deephaven.io>
2024-04-25 11:55:23 -0500

    Local url tweak

--- src/util.ts
+++ src/util.ts
@@ -163,7 +163,8 @@ export function polyfillDh() {
 
   // This is needed to mimic running in a local http browser environment when
   // making requests to the server. This at least impacts websocket connections.
-  // Not sure if it is needed for other requests.
+  // Not sure if it is needed for other requests. The url is an arbitrary
+  // non-https url just to make it stand out in logs.
   // @ts-ignore
-  global.window.location = new URL('http://localhost:8080/');
+  global.window.location = new URL('http://dh-in-vscode.localhost/');
 }
